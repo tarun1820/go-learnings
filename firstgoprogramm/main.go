1c7e2cf289fd6642119bc8d893298cd01a0bff9d
tarun1820 <edupugantitarunvenkatasai@gmail.com>
2025-09-04 11:19:06 +0530

    first file

---
+++ firstgoprogramm/main.go
@@ -0,0 +1,19 @@
+// Build a Go program:
+
+// While inside a program directory, type:
+// go build main.go
+// Run a Go program:
+
+// While inside a program directory, type:
+// go run main.go
+
+// If you create other files and run them all, you can use this command:
+// go run .
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hi! I want to be a Gopher!")
+}
